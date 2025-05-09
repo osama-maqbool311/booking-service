43c68d4177827ca9fd5b1bf4f646d0060f827659
Muhammad Osama Maqbool <mosama.maqbool@systemsltd.com>
2025-05-09 08:59:39 +0500

    add resilence for bookSeats post request

--- src/main/java/com/daas/controller/BookingController.java
+++ src/main/java/com/daas/controller/BookingController.java
@@ -3,10 +3,12 @@ package com.daas.controller;
 import com.daas.dto.request.BookingRequest;
 import com.daas.entity.Booking;
 import com.daas.service.BookingService;
+import io.smallrye.faulttolerance.api.RateLimit;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.faulttolerance.Fallback;
 
 @Path("/booking")
 public class BookingController {
@@ -17,6 +19,8 @@ public class BookingController {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
+    @RateLimit(value = 2, window = 1)
+    @Fallback(fallbackMethod = "fallbackBooking")
     public Response bookSeats(final BookingRequest req) {
         Booking booking = bookingService.createBooking(req);
         return Response.status(Response.Status.CREATED)
@@ -30,6 +34,12 @@ public class BookingController {
         Booking booking = bookingService.bookingById(id);
         return Response.status(Response.Status.CREATED)
                 .entity(booking)
+                .build();
+    }
+
+    public Response fallbackBooking(BookingRequest req) {
+        return Response.status(Response.Status.TOO_MANY_REQUESTS)
+                .entity("Too many requests - please try again later.")
                 .build();
     }
 }
